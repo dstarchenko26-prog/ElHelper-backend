1ba4258d3ba7d9ab3c245470e99ed0d6b7fc7534
dstarchenko26-prog <denys.starchenko.oi.2023@lpnu.ua>
2026-01-04 12:22:23 +0200

    Update AdminRequest.java

--- src/main/java/ua/nulp/elHelper/entity/user/AdminRequest.java
+++ src/main/java/ua/nulp/elHelper/entity/user/AdminRequest.java
@@ -12,6 +12,7 @@ import java.time.Instant;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class AdminRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
